0689a8675eaeabc59908803d2d8de1e07179c6d5
Jorge Hevia <jmhev@outlook.com>
2018-05-06 19:15:11 +0200

    Changed the function from precision to recall

--- metrics.py
+++ metrics.py
@@ -9,41 +9,41 @@ class Metrics:
         # GLOBAL VARIABLES
         self.trelFile = '2010.union.trel'
     '''
-        Cut precision 5, 10
+        Cut Recall 5, 10
         This metric will check how many of the
         docs are relevant
     '''
     '''
-        Name: cutPrecision
+        Name: cutRecall
         In: array with the file names of the documents returned
             ¡¡¡ SORTED BY NAME !!!
-        Out: returns a string showing the cut precision
+        Out: returns a string showing the cut Recall
         Function: given an array of filenames, it will check the
                 relevance of the files and then will return an string
-                showing the cut precision 5, 10
+                showing the cut Recall 5, 10
     '''
 
-    # auxiliar function so cutPrecision can use multiple processes
+    # auxiliar function so cutRecall can use multiple processes
     def checkRelevance(self, file):
         trel = open(self.trelFile, 'r')
         for line in trel:
             if file in line:
                 return str(file), int(line.split()[3])
 
-    def cutPrecision(self, files):
+    def cutRecall(self, files):
         # checks the relevance of the Files
         pool = multiprocessing.Pool()
         relevance = [pool.map(self.checkRelevance, files)]
-        # saves the precision, which is relevantFiles / retrieved
-        precision5 = 0.0   # first 5 files
-        precision10 = 0.0  # first 10 files
+        # saves the Recall, which is relevantFiles / retrieved
+        Recall5 = 0.0   # first 5 files
+        Recall10 = 0.0  # first 10 files
         count = 0
         for (doc, rel) in sorted(relevance[0]):
             if rel >= 1 and count < 5:
-                precision5 = precision5 + 0.2
+                Recall5 = Recall5 + 0.2
             if rel >= 1 and count < 10:
-                precision10 = precision10 + 0.1
+                Recall10 = Recall10 + 0.1
             count += 1
-        precision = "{'precision5': " + str(precision5) + \
-                     ", 'precision10': " + str(precision10) + "}"
-        return precision
+        Recall = "{'Recall5': " + str(Recall5) + \
+                     ", 'Recall10': " + str(Recall10) + "}"
+        return Recall
